a9d8565124c653d778ca6730058d405a00e0a96c
Rustam Batyrshin <truezorvis@localhost>
2022-12-03 16:08:44 +0500

    Changed 5.2_8

--- stepik_py_oop/ch04/5.2/5.2_8.py
+++ stepik_py_oop/ch04/5.2/5.2_8.py
@@ -1,15 +1,24 @@
 class Rect:
     def __init__(self, x, y, width, height):
-        if type(x) not in (int, float) or type(y) not in (int, float) or width <= 0 or height <= 0:
-            raise ValueError('некорректные координаты и параметры прямоугольника')
         self._x = x
         self._y = y
         self._width = width
         self._height = height
 
+    def __setattr__(self, key, value):
+        if type(value) not in (int, float):
+            raise ValueError('некорректные координаты и параметры прямоугольника')
+        if key in ('_width', '_height') and value <= 0:
+            raise ValueError('некорректные координаты и параметры прямоугольника')
+        object.__setattr__(self, key, value)
+
     def is_collision(self, rect):
-        pass
-        # raise TypeError('прямоугольники пересекаются')
+        if not isinstance(rect, Rect):
+            raise TypeError("аргументом должен быть объект класса Rect")
+
+        if not (self._x + self._width < rect._x or rect._x + rect._width < self._x or \
+                self._y + self._height < rect._y or rect._y + rect._height < self._y):
+            raise TypeError('прямоугольники пересекаются')
 
 
 lst_rect = [
@@ -22,13 +31,15 @@ lst_rect = [
 
 lst_not_collision = []
 for i in range(len(lst_rect)):
-    for j in range(i+1, len(lst_rect)):
-        try:
-            lst_rect[i].is_collision(lst_rect[j])
-        except TypeError:
-            pass
-        else:
-            lst_not_collision.append(lst_rect[i])
+    flag = True
+    for j in range(len(lst_rect)):
+        if i != j:
+            try:
+                lst_rect[i].is_collision(lst_rect[j])
+            except TypeError:
+                flag = False
+                break
+    if flag:
+        lst_not_collision.append(lst_rect[i])
 
-print(lst_not_collision)
 print('end')
