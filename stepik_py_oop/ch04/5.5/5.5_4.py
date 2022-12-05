a6a5d01a6023d677546cdfddcf0a1434262ece88
Rustam Batyrshin <truezorvis@localhost>
2022-12-05 13:49:35 +0500

    Added 5.5_3, 5.5_4, 5.5_5

---
+++ stepik_py_oop/ch04/5.5/5.5_4.py
@@ -0,0 +1,27 @@
+class ConnectionError(Exception):
+    pass
+
+
+class DatabaseConnection:
+    def __init__(self):
+        self._fl_connection_open = False
+
+    def connect(self, login=None, password=None):
+        self._fl_connection_open = True
+        raise ConnectionError
+
+    def close(self):
+        self._fl_connection_open = False
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+        return False
+
+
+with DatabaseConnection() as conn:
+    pass
+
+print('end')
